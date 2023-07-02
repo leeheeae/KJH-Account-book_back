1632cbcd4b7a77366083d9f309c695de4465433b
Park JeongBin <ybaram40@gmail.com>
2023-07-02 22:23:11 +0900

    Config 설정 완료

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ConfigModule } from '@nestjs/config';
+import {
+  DEV,
+  ENV_FILE_NAME,
+  ENV_FILE_NAME_DEV,
+} from './common/constants/common.constant';
+import * as Joi from 'joi';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath:
+        process.env.NODE_ENV === DEV ? ENV_FILE_NAME_DEV : ENV_FILE_NAME,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().required(),
+        SWAGGER_USER: Joi.string().required(),
+        SWAGGER_PASSWORD: Joi.string().required(),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
