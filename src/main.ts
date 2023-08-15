103beeca1eb3d674151adc1351cce0e1b17b40d5
cafe small house <ybaram40@gmail.com>
2023-08-15 09:17:58 +0900

    dev 설정 완료

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import * as express from 'express';
+import { join } from 'path';
+import { BACKEND_URL, PORT } from './common/constants/global.constant';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
+  if (process.env.SWAGGER_MODE === 'true') {
+    const docs = require('../../swagger.json');
+    docs.servers = [{ url: 'http://localhost:8000' }];
+    SwaggerModule.setup('swagger', app, docs);
+  }
+  app.use('/files', express.static(join(__dirname, '../files')));
+  const start = () => console.log(`Server Start! ${BACKEND_URL}`);
+  await app.listen(PORT, start);
 }
 bootstrap();
